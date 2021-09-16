6e7db0908e1c282cf1d4bf4c9563fc5780ba5132
Sujay Chand <sujaygchand@gmail.com>
2021-09-16 22:57:31 +1200

    Removed reference to data folder

--- NZMartialArtsGymFinderWeb/Startup.cs
+++ NZMartialArtsGymFinderWeb/Startup.cs
@@ -8,7 +8,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using NZMartialArtsGymFinderWeb.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
