17a25cefb9262ea84f5e3e11f591b5c1d0188a59
Sujay Chand <sujaygchand@gmail.com>
2021-09-19 01:50:38 +1200

    Added API links to static details

---
+++ NZMartialArtsGymFinderWeb/Utilities/StaticDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NZMartialArtsGymFinderWeb.Utilities
+{
+	public class StaticDetails
+	{
+		public static string APIBaseUrl = "https://n8w073lxid.execute-api.ap-southeast-2.amazonaws.com/";
+		public static string RegionAPIPath = APIBaseUrl + "api/Region/";
+		public static string GymAPIPath = APIBaseUrl + "api/Gym/";
+		public static string MartialArtAPIPath = APIBaseUrl + "api/MartialArt/";
+		public static string UserAPIPath = APIBaseUrl + "api/user/";
+
+		public static string Alert = "alert";
+	}
+}
