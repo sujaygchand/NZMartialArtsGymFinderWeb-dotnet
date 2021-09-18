e02c82531d5d15a5a7f2ce3eb29995a4e06a741f
Sujay Chand <sujaygchand@gmail.com>
2021-09-18 16:25:59 +1200

    Created base repository class

---
+++ NZMartialArtsGymFinderWeb/Repository/Repository.cs
@@ -0,0 +1,44 @@
+﻿using NZMartialArtsGymFinderWeb.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NZMartialArtsGymFinderWeb.Repository
+{
+	public class Repository<T> : IRepository<T> where T : class
+	{
+		public readonly IHttpClientFactory _clientFactory;
+
+		public Repository(IHttpClientFactory clientFactory)
+		{
+			_clientFactory = clientFactory;
+		}
+
+		public Task<bool> CreateAsync(string url, T objToCreate, string token)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<bool> DeleteAsync(string url, int id, string token)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IEnumerable<T>> GetAllAsync(string url, string token)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<T> GetAsync(string url, int id, string token)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<bool> UpdateAsync(string url, T objToUpdate, string token)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
